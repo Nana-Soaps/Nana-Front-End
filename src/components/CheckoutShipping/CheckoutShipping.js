63453455d94513f0f4f7609bdc1f060511560288
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-10-12 09:08:09 -0400

    added validation error msg to shipping options

--- src/components/CheckoutShipping/CheckoutShipping.js
+++ src/components/CheckoutShipping/CheckoutShipping.js
@@ -6,10 +6,34 @@ import CartSummary from "./components/CartSummary";
 import Contact from "./components/Contact";
 import "../../styles/CheckoutShipping.scss";
 import chevronLeftClose from "../../assets/chevron-left-close.svg";
+import * as yup from "yup";
 import { connect } from "react-redux";
 import { setOrder, setShippingOptions } from "../../actions";
 
 function CheckoutShipping(props) {
+  const formSchema = yup.object().shape({
+    first_name: yup.string().trim().required("Required"),
+    last_name: yup.string().trim().required("Required"),
+    email: yup.string().email("Must be valid email").required("Required"),
+    shipping_city: yup.string().trim().required("Required"),
+    shipping_state: yup.string().trim().min(1, "Required").required("Required"),
+    shipping_address: yup.string().trim().required("Required"),
+    shipping_zip: yup
+      .string()
+      .trim()
+      .min(5, "Must be 5 characters")
+      .max(5, "Must be 5 characters")
+      .required("Required"),
+    shipping_apartment: yup.string(),
+    shipping_country: yup
+      .string()
+      .trim()
+      .min(1, "Required")
+      .required("Required"),
+    notes: yup.string(),
+    shipping_cost: yup.number(),
+    shipping_id: yup.number(),
+  });
   useEffect(() => {
     axios
       .get("https://nanasoapsbackend.herokuapp.com/api/orders/shipping-options")
@@ -22,7 +46,13 @@ function CheckoutShipping(props) {
       });
   }, []);
   const history = useHistory();
+  const [mainError, setMainError] = useState(false);
+  const checkIfValid = async (data) => {
+    const valid = await formSchema.isValid(data);
+    return valid;
+  };
   const onChange = (e) => {
+    setMainError(false);
     const { name, value, checked } = e.target;
     const currentOrder = props.order;
     const cost = e.target.getAttribute("data-cost");
@@ -32,21 +62,42 @@ function CheckoutShipping(props) {
       props.setOrder({ ...currentOrder, [name]: null, shipping_cost: 0 });
     }
   };
-  const handleBack = () => {
+  const handleBack = (e) => {
+    e.preventDefault();
     history.goBack();
     // console.log("back");
   };
 
-  const handlePayment = () => {
-    history.push("/checkout/payment");
+  const handlePayment = async (e) => {
+    e.preventDefault();
+    const valid = await checkIfValid(props.order);
+    console.log(valid);
+    if (valid) {
+      history.push("/checkout/payment");
+    } else {
+      setMainError(true);
+    }
   };
   return (
     <div className="checkoutShipping py-5">
       <div className="container">
-        <form>
+        <form style={{ position: "relative" }}>
           <h5 className="text-start">Cart Summary</h5>
           <CartSummary />
           <Contact />
+          {mainError && (
+            <p
+              style={{
+                position: "absolute",
+                color: "red",
+                width: "100%",
+                transform: "translateY(-20px)",
+              }}
+            >
+              Please select a shipping option
+            </p>
+          )}
+
           <h5 className="text-start">Shipping Options</h5>
           {props.shippingOptions.map((option) => (
             <ShippingCheckbox onChange={onChange} option={option} />
