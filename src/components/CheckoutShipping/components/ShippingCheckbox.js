dfdc13fce75e5c55125987db3bde8c19e5407046
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-10-12 08:45:34 -0400

    shipping options work

--- src/components/CheckoutShipping/components/ShippingCheckbox.js
+++ src/components/CheckoutShipping/components/ShippingCheckbox.js
@@ -5,19 +5,19 @@ function ShippingCheckbox(props) {
 
   return (
     <div className="checkboxWrap d-flex justify-content-between align-items-center px-3 border mb-2">
-      <label className="d-flex flex-row-reverse justify-content-start align-items-center inputWrap">
+      <label className="d-flex flex-row-reverse justify-content-end align-items-center inputWrap ">
         <div className="ps-3">
           <p className="m-0 shippingName text-start">{option.shipping_name}</p>
           <p className="m-0 shippingDescription text-start">
-            3-7 Business Days
+            {option.shipping_description}
           </p>
         </div>
         <input
-          type="checkbox"
+          type="radio"
           onChange={onChange}
-          value={1}
+          value={option.shipping_id}
           name="shipping_id"
-          data-cost={7}
+          data-cost={option.cost}
         />
       </label>
       <p className="m-0 cost">${option.cost.toFixed(2)}</p>
