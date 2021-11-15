54a096d5ed5bb8a33165d8ee522339c5261908cb
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-11-14 19:33:13 -0500

    added carousel to homepage

---
+++ src/components/MainDash/components/BestsellerItem.js
@@ -0,0 +1,80 @@
+import React from "react";
+import soapImg from "../../../assets/soap-item.png";
+import bagAdd from "../../../assets/bag-add.svg";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setBag, toggleCartOpen } from "../../../actions";
+import { disableBodyScroll } from "body-scroll-lock";
+
+function BestsellerCard(props) {
+  const { product } = props;
+
+  const handleAddToBag = (e) => {
+    e.preventDefault();
+    let itemExists = false;
+    let currentProduct = null;
+    props.bag.forEach((item) => {
+      if (item.name == product.name) {
+        itemExists = true;
+        currentProduct = item;
+      }
+    });
+
+    if (itemExists) {
+      const newBag = props.bag.map((item) => {
+        if (item.name == currentProduct.name) {
+          item.quantity += 1;
+        }
+        return item;
+      });
+      console.log(newBag);
+      props.setBag(newBag);
+    } else {
+      props.setBag([...props.bag, { ...product, quantity: 1 }]);
+    }
+    props.toggleCartOpen(true);
+    disableBodyScroll(true);
+  };
+  return (
+    <div className="bestsellerCard p-1">
+      <div className="itemWrap">
+        <Link
+          to={{ pathname: "/shop/item", state: { product: product } }}
+          className="text-decoration-none"
+        >
+          <div className="imgWrap border rounded d-flex justify-content-center align-items-center">
+            <img
+              src={`https://nana-soaps-products.s3.us-east-2.amazonaws.com/${product.product_id}`}
+              className="img-fluid"
+              alt={`${product.name}`}
+            />
+          </div>
+        </Link>
+        <div className="textWrap rounded">
+          <Link
+            to={{ pathname: "/shop/item", state: { product: product } }}
+            className="text-decoration-none"
+          >
+            <h5 className="name px-1">{product.name}</h5>
+            <h5 className="price">${product.price.toFixed(2)}</h5>
+          </Link>
+          <div
+            className="d-flex justify-content-center addToBagWrap"
+            onClick={handleAddToBag}
+          >
+            <h5 className="me-2 addText">Add To Bag</h5>
+            <img src={bagAdd} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+export default connect(mapStateToProps, { setBag, toggleCartOpen })(
+  BestsellerCard
+);
--- src/components/MainDash/components/Bestsellers.js
+++ src/components/MainDash/components/Bestsellers.js
@@ -1,19 +1,77 @@
-import React from "react";
-import ItemCard from "./ItemCard.js";
-
+import React, { useRef, useState, useEffect } from "react";
+import { connect } from "react-redux";
+import BestsellerItem from "./BestsellerItem.js";
+import chevronLeft from "../../../assets/chevron-left-large.svg";
+import chevronRight from "../../../assets/chevron-right-large.svg";
 function Bestsellers(props) {
+  const [offset, setOffset] = useState(0);
+  const [cardsOnScreen, setCardsOnScreen] = useState(0);
+  const cardsWrap = useRef(null);
+  const fixedWrap = useRef(null);
+
+  const setCardNumber = () => {
+    const width = window.innerWidth;
+    if (width > 1200) {
+      setCardsOnScreen(5);
+    } else if (width <= 1200 && width > 990) {
+      setCardsOnScreen(4);
+    } else if (width <= 990 && width > 767) {
+      setCardsOnScreen(3);
+    } else if (width <= 767) {
+      setCardsOnScreen(2);
+    }
+  };
+
+  window.onresize = setCardNumber;
+
+  useEffect(() => {
+    setCardNumber();
+  }, []);
+
+  const slideLeft = () => {
+    const bestsellerCard = cardsWrap.current.children[0];
+    const cardWidth = bestsellerCard.scrollWidth;
+    const maxWidth = cardsWrap.current.scrollWidth - cardWidth * cardsOnScreen;
+
+    if (offset < (-1 * cardWidth) / 2) {
+      cardsWrap.current.style.transform = `translateX(${offset + cardWidth}px)`;
+      setOffset(() => offset + cardWidth);
+    } else {
+      cardsWrap.current.style.transform = `translateX(${-1 * maxWidth}px)`;
+      setOffset(() => -1 * maxWidth);
+    }
+  };
+  const slideRight = () => {
+    const bestsellerCard = cardsWrap.current.children[0];
+    const cardWidth = bestsellerCard.scrollWidth;
+    const maxWidth = cardsWrap.current.scrollWidth - cardWidth * cardsOnScreen;
+    console.log(maxWidth, -1 * offset);
+    if (offset > -1 * (maxWidth - cardWidth / 2)) {
+      cardsWrap.current.style.transform = `translateX(${offset - cardWidth}px)`;
+      setOffset(() => offset - cardWidth);
+    } else {
+      cardsWrap.current.style.transform = `translateX(0px)`;
+      setOffset(0);
+    }
+  };
   return (
     <div className="bestsellers py-5">
       <div className="container">
         <div>
           <h1 className="bestsellers-header">Explore our Bestsellers</h1>
-          <div className="content">
-            <div className="cards-wrap">
-              <ItemCard />
-              <ItemCard />
-              <ItemCard />
-              <ItemCard />
-              <ItemCard />
+          <div className="d-flex justify-content-center align-items-center content-wrap">
+            <div className="arrow-wrap-left" onClick={slideLeft}>
+              <img src={chevronLeft} alt="chevron left" />
+            </div>
+            <div className="fixed-wrap" ref={fixedWrap}>
+              <div className="cards-wrap" ref={cardsWrap}>
+                {props.products.map((product) => (
+                  <BestsellerItem product={product} />
+                ))}
+              </div>
+            </div>
+            <div className="arrow-wrap-right" onClick={slideRight}>
+              <img src={chevronRight} alt="chevron right" />
             </div>
           </div>
         </div>
@@ -22,4 +80,10 @@ function Bestsellers(props) {
   );
 }
 
-export default Bestsellers;
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+
+export default connect(mapStateToProps)(Bestsellers);
