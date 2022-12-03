923d8ba5b8c216d1608674d491798cf05c0599ba
Marat Kotik <mkotik97@gmail.com>
2022-12-03 14:19:28 -0500

    Added our story img

--- src/components/OurStory/OurStoryUpper.js
+++ src/components/OurStory/OurStoryUpper.js
@@ -3,7 +3,13 @@ import React from "react";
 function OurStoryUpper(props) {
   return (
     <div className="upper">
-      <div className="imgWrap border"></div>
+      <div className="imgWrap border" style={{ overflow: "hidden" }}>
+        <img
+          src="https://bloximages.chicago2.vip.townnews.com/tdn.com/content/tncms/assets/v3/editorial/e/99/e99e2fda-0f64-11e4-aa88-001a4bcf887a/53caa239f13a0.preview-699.jpg?crop=679%2C509%2C10%2C0&resize=1200%2C900&order=crop%2Cresize"
+          alt="lady holding soap"
+          style={{ height: "100%", transform: "translateX(-140px)" }}
+        />
+      </div>
       <div className="textWrap">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquam
